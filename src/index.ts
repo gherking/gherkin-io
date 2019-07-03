fb9d658f0cd74a783f7634af00a27b6f4bef1129
Laszlo_Szikszai <Laszlo_Szikszai@epam.com>
2019-07-03 22:26:21 +0200

    Implementation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { Document } from "gherkin-ast";
 
 export const read = async (pattern: string): Promise<Document[]> => {
+    if (!pattern) {
+        throw new Error("[gherkin-io] [read] pattern must be set!");
+    }
     return [];
 }
